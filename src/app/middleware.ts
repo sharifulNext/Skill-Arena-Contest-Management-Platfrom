7a9f805c2f6901c0c9ae9f09edb415b5d5bd9321
Shariful Islam <sharifulislam242248@gmail.com>
2026-05-27 22:33:53 +0600

    all fix done

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,4 +1,10 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login", // আপনার লগইন পেজের পাথ
+  },
+});
 
 export const config = {
   matcher: ["/dashboard/:path*", "/profile/:path*", "/settings/:path*"],
